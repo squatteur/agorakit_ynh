2ca9b25d7c0b58636f41766b319cf30a221e215a
bestel <squatteur@users.noreply.github.com>
2020-12-02 01:26:50 +0100

    Update _common.sh
    imap

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,7 +9,9 @@
 pkg_dependencies="openssl"
 
 YNH_PHP_VERSION=7.3
-extra_php_dependencies="php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-ldap php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-common php${YNH_PHP_VERSION}-gd"
+extra_php_dependencies="php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-ldap \
+php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-bcmath php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-common php${YNH_PHP_VERSION}-gd \
+php${YNH_PHP_VERSION}-imap"
 
 #=================================================
 # PERSONAL HELPERS
